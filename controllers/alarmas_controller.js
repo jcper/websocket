bb13bf8cf3fb905c66870b87bb4b27d023e1cabd
jcper <informaticajcper@gmail.com>
2016-12-28 09:29:19 +0100

    se añade controllers

---
+++ controllers/alarmas_controller.js
@@ -0,0 +1,6 @@
+var models = require('../models/models.js');
+
+GET  /alarmas/show
+exports.show= function(req, res){
+ res.render('alarmas/show');
+  };
---
+++ controllers/equipos_controllers.js
@@ -0,0 +1,9 @@
+var models = require('../models/models.js');
+
+
+/GET  /equipos/show
+exports.show= function(req, res){
+ res.render('equipos/show');
+  };
+
+/
